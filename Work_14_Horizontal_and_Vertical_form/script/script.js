946a354faae86d2a610615060d86ad1be10ad68b
GauravSolo <gs25802582@gmail.com>
2022-06-11 23:12:56 +0530

    create horizontal and vertical pop up forms

---
+++ Work_14_Horizontal_and_Vertical_form/script/script.js
@@ -0,0 +1,36 @@
+// document.querySelectorAll('input[type="text"]').forEach(element => {
+    
+//     element.addEventListener('keydown',(e)=>{
+
+//         var string = e.target.value;
+        
+//         var key = e.keyCode || e.charCode;
+//     if( key == 8 ){
+    	
+//         if(string.length <= 4)
+//         {
+//             e.target.value = "SPHfgf";
+//             console.log(string.length);
+//         }
+//         console.log('backspace')
+//     }
+
+//     console.log(key);
+
+//     })
+
+// });
+// jQuery(function($) { // DOM ready and $ alias secured
+
+//     $('.sph').on('keydown', function(e){
+//         var str = e.target.value;
+//         var len = str.length;
+//         if(len <= 3)
+//         {
+//             e.target.value = "SPH";
+//             console.log(len,e.target.value);
+//         }
+        
+//     });
+  
+//   });
